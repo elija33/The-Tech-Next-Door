0c9e491f1575369b1d04cd64a5b6031d288b18bf
Eli <amponsaeli@gmail.com>
2017-07-27 23:25:57 -0700

    TheTechNextDoor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
 /*
-	ZeroFour by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	This is Elija code and work
 */
 
 (function($) {
